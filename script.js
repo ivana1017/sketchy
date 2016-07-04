241f2463f0166f41b4c5b6e27c9e7366d72b9661
Ivana Rodriguez <ivana1017@gmail.com>
2016-07-04 09:40:03 -0400

    Add generate grid function

---
+++ script.js
@@ -0,0 +1,23 @@
+$(document).ready(function(){
+	var dim = prompt("Please enter your desired sketchpad dimension:")
+	generateGrid(dim);
+});
+
+function generateGrid(dimension){
+	var size = dimension * dimension;
+	var height = 0;
+	var width = 0;
+	var count = 0;
+
+	while(count < size){
+		count++;
+
+		if(width == dimension && height != dimension){
+			$('<div></div>').addClass('clear').appendTo('.container');
+			height++;
+			width = 0;
+		}
+		$('<div></div>').addClass('stack').appendTo('.container');
+		width++;
+	}
+}
